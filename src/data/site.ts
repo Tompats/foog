7145f18137f5b6428d85ffce8600833a269d6a7e
Thomas Patsanis <tompats1998@gmail.com>
2025-11-27 23:02:09 +0200

    Fix some wordings

--- src/data/site.ts
+++ src/data/site.ts
@@ -56,7 +56,7 @@ export const siteMetadata: SiteMetadata = {
   locale: "el_GR",
   language: "el",
   siteUrl: "https://tompats.github.io/foog",
-  contactEmail: "foog@gmail.com",
+  contactEmail: "fooggiannitson@gmail.com",
   phone: "+30 23820 00000",
   address: {
     street: "Ανοιχτού Θεάτρου 8",
